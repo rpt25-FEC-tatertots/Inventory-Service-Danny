56f16a82ca987eb7b348515d30001723670e287c
Danny Harmon <dannyaharmon@gmail.com>
2021-01-25 08:58:13 -0800

    add new component

---
+++ client/src/components/infoContainer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-compenents';
+
+const Name = styled.span`
+font-size: 20px;
+`;
+const Price = styled.span`
+font-size: 15px;
+`;
+const Abbreviation = styled.span`
+font-size: 20px;
+`;
+const infoContainer = (props) => (
+  <div>
+    <Name>{props.item.colorName}</Name>
+    <Price>{props.item.price}</Price>
+    <Abbreviation>{props.item.abbreviation} |{props.item.productID}</Abbreviation>
+  </div>
+);
+
+export default infoContainer;
