ea6763298fda5ad9b237941a5034e9d632f00c79
Danny Harmon <dannyaharmon@gmail.com>
2021-02-04 10:27:19 -0800

    update styling to best so far

--- client/src/components/thumbnailContainer.jsx
+++ client/src/components/thumbnailContainer.jsx
@@ -4,36 +4,23 @@ import styled from 'styled-components';
 const ImageContainer = styled.div`
 display: flex;
 flex-direction: row;
-flex-shrink: 0;
-flex-wrap: wrap;
+flex-wrap: no-wrap;
 justify-content: center;
 align-items: center;
-padding: 0 4rem;
-max-width: 90rem;
-@media (max-width: 575px) {
-  padding-right: 40px;
-  justify-content: flex-start;
-  flex-wrap: no-wrap;
-  padding-left: 20px;
-}
+width: 100%;
 `;
-const ImageWrapper = styled.div`
+const ImageWrap = styled.div`
 position: relative;
-background: blue;
-flex-basis: 12.5%;
-padding: .4rem;
-@media (max-width: 575px) {
-  flex-basis: 33.3333%;
-}
+width: 100px;
+height: 100px;
+margin: 1px;
 `;
 const Image = styled.img`
 position: absolute;
-width: 100%;
+height: 100px;
+width: 100px;
 border-radius: 10px;
 opacity: ${(props) => props.outOfStock ? .3 : 1};
-@media (min-width: 972px) {
-  width: 75%
-})
 `;
 const SvgContainer = styled.div`
 position: absolute;
@@ -51,7 +38,7 @@ const thumbnailContainer = ({ onThumbClick, item, activeSize, activeColor }) => 
       sizeObj.size === activeSize && sizeObj.quantity === 0
     ));
     return (
-      <ImageWrapper key={color.thumbnail}>
+      <ImageWrap key={color.thumbnail}>
         <Image
           src={color.thumbnail}
           alt="thumbnail"
@@ -59,7 +46,7 @@ const thumbnailContainer = ({ onThumbClick, item, activeSize, activeColor }) => 
           outOfStock={outOfStock[0] ? true : false}
         />
         <SvgContainer hidden={activeColor === color ? false : true} />
-      </ImageWrapper>
+      </ImageWrap>
     );
   });
 
