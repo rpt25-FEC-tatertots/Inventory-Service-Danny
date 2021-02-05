802401f0bbff92822c3f9023bc15fde46c0669a8
Danny Harmon <dannyaharmon@gmail.com>
2021-02-05 01:14:57 -0800

    Style Buttons

--- client/src/components/buttonContainer.jsx
+++ client/src/components/buttonContainer.jsx
@@ -2,29 +2,52 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Button = styled.div`
-padding: .4rem 2rem;
-margin: .4;
+padding: .4rem;
+font-weight: 700;
+border-style: solid;
+border-color: black
+border-width: 4px;
 cursor: pointer;
-border-radius: 15px;
+border-radius: 30px;
+background-color: white;
+color: black;
+transition: transform .4s ease;
+:hover {
+  transform: scale(1.05);
+}
+`;
+const BuyButton = styled.div`
+padding: .4rem;
+border-color: ${({ buy }) => buy === 'Buy' ? 'black' : '#fa4616'};
+cursor: pointer;
+border-radius: 30px;
 background-color: ${({ buy }) => buy === 'Buy' ? 'black' : '#fa4616'};
 color: white;
-transition: transform .3s ease;
+transition: transform .4s ease;
 :hover {
   transform: scale(1.05);
 }
 `;
 const ButtonWrapper = styled.div`
-width: 100%
+width: 300px;
+margin: .5rem;
+text-align: center;
+`;
+const WrapperWrapper = styled.div`
+margin-top: 1rem;
 display: flex;
 flex-direction: column;
-justify-content: center;
 align-items: center;
 `;
 const ButtonContainer = ({ buy }) => (
+  <WrapperWrapper>
   <ButtonWrapper>
-    <Button id="buy">{buy}</Button>
+    <BuyButton id="buy">{buy}</BuyButton>
+  </ButtonWrapper>
+  <ButtonWrapper>
     <Button>Browse Used Fleece</Button>
   </ButtonWrapper>
+  </WrapperWrapper>
 );
 
 export default ButtonContainer;
