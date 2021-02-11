aa2ca6e53420c02cc4502e059a798bbafb90461c
Danny Harmon <dannyaharmon@gmail.com>
2021-02-11 11:24:31 -0800

    Default to port 5004 if there are no env variables

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
 const App = require('./server.js');
 
 // eslint-disable-next-line no-unused-expressions
-App.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
+App.listen(process.env.PORT || 5004, () => console.log(`Server is listening on port ${process.env.PORT}`));
